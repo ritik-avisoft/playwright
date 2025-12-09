c9ab9723fb3e99ae7e46e4a7faa3fdd6332113d9
ritik-avisoft <ritik.ranjan@avisoft.io>
2025-12-09 18:28:36 +0530

    Final Day03

--- KnowledgeWere/elements.py
+++ KnowledgeWere/elements.py
@@ -15,9 +15,9 @@ from playwright.sync_api import Page,expect
 #     page.locator('id=peraddress').fill("Patna, Bihar, India 804401")
 
 #     # page.pause()
+#     page.pause()
 #     page.get_by_role("button", name='Submit').click()   #After submitting the op get's disappear within sec....
 #     print("test_box Done..")
-#     page.pause()
 
 # def test_check_box(page:Page):
 #     page.goto("https://www.knowledgeware.in/Automation/check.html#")
@@ -41,23 +41,31 @@ from playwright.sync_api import Page,expect
 #     print(your_choice.text_content())
 
 
-def test_Xpath(page:Page):
-    page.goto("https://www.knowledgeware.in/Automation/web.html")
+# def test_Xpath(page:Page):
+#     page.goto("https://www.knowledgeware.in/Automation/web.html")
 
-    salary_elements=page.locator('//tr/td[3]')
-    salary_text=salary_elements.all_text_contents()
-    print("all salary are now in salary_text....")
-    max_sal=0
+#     salary_elements=page.locator('//tr/td[3]')
+#     salary_text=salary_elements.all_text_contents()
+#     print("all salary are now in salary_text....")
+#     max_sal=0.00
 
-    for sal in salary_text:
-        sal=float(sal)
-        if sal>max_sal:
-            max_sal=sal
-    print("We found the max salary....")
-    max_salary_text = f"{max_sal:.2f}"
-    print("we convert sal into text again from int")
+#     for sal in salary_text:
+#         sal = sal.strip()
+#         if not sal:
+#             continue
 
-    page.locator(f"//td[text()='{max_salary_text}']/../td[5]").click()
-    print("we clicked on the read more...")
-    page.pause()
+#         try:
+#             sal=float(sal)
+#             if sal>max_sal:
+#                 max_sal=sal
+#         except ValueError:
+#             continue
+
+#     print("We found the max salary....")
+#     max_salary_text = f"{max_sal:.2f}"
+#     print("we convert sal into text again from int")
+
+#     page.locator(f"//td[text()='{max_salary_text}']/../td[5]").click()
+#     print("we clicked on the read more...")
+#     page.pause()
     
